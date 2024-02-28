309f9045485ea1f8186388b607c43d8c16c23833
Mathis Burger <handymathis@gmail.com>
2024-02-28 21:21:02 +0100

    [BUGFIX] Fixed workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {unoptimized: true},
+    experimental: {images: {unoptimized: true}},
+    basePath: "/typespeed",
     output: 'export'
-};
+}
+module.exports = nextConfig
 
 export default nextConfig;
