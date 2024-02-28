527f4070a4ad2d0eaee31baf6ca93b772feb40fa
Mathis Burger <handymathis@gmail.com>
2024-02-28 20:57:42 +0100

    [TASK] Added some more simple languages

--- app/languages.ts
+++ app/languages.ts
@@ -6,10 +6,18 @@ export interface ProgrammingLanguage {
 export const languages: ProgrammingLanguage[] = [
     {
         name: 'Typescript',
-        items: ['let', 'var', 'export', 'interface', 'type', 'class', 'const', 'const name = () => {}', 'string', 'number']
+        items: ['let', 'var', 'export', 'interface', 'type', 'class', 'const', 'const name = () => {}', 'string', 'number', 'setTimeout', 'setInterval', 'break', 'continue', 'for', 'while', 'async', 'await', 'function', 'Omit', 'Pick', 'concat', 'push', 'private', 'any', 'string[]', 'number[]', '{', '}']
+    },
+    {
+        name: 'Java',
+        items: ['String', 'Integer', 'int', 'long', 'double', 'public', 'static', 'void', 'String[]', 'new', '{', '}', 'for', 'while', 'do', 'interface', 'class', 'protected', 'this']
+    },
+    {
+        name: 'Rust',
+        items: ['for', 'let', 'mut', 'dyn', 'Box', 'while', 'Option', 'Result', 'std::', 'use', 'mod', 'fn', 'pub', 'pub(crate)']
     }
 ];
 
 export const words: string[] = [
-    'variable', 'data', 'hidden', 'consider', 'status'
+    'variable', 'data', 'hidden', 'consider', 'status', 'value', 'amount', 'start', 'bread', 'container', 'compile', 'memory', 'stack', 'heap'
 ];
