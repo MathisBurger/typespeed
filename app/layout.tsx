92fcf0e131d786696ff60d752b9b310b5665557a
Mathis Burger <handymathis@gmail.com>
2024-02-28 21:06:37 +0100

    [BUGFIX] Fixed build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Inter } from "next/font/google";
+import {Suspense} from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Suspense>
+          {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
